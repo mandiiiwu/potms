6f63bd702a9caa338e20433cd2e773a01bc0b08c
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-04-28 12:35:21 -0400

    Create april2025.py

---
+++ april2025.py
@@ -0,0 +1,22 @@
+n = int(input())
+nums = sorted(list(map(int, input().split())))
+print(nums)
+
+newlist = []
+
+if n % 2 == 1:
+    n += 1
+
+for _ in range(int(n/2)):
+    newlist.append(max(nums) - min(nums))
+    nums.remove(min(nums))
+    try:
+        nums.remove(max(nums))
+    except: pass
+    print(newlist)
+
+final = 0
+for x in newlist:
+    final += x
+
+print(final)
